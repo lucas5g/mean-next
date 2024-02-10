9e2d98071d62460ec1341cb80380680eb174db37
lucas.sousa <lucas.sousa@codificar.com.br>
2024-02-10 20:24:01 -0300

    remove comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,12 +11,8 @@ export interface WordInterface {
 
 export default async function Home() {
 
-  // await setTimeout(30000)
   const res = await fetch(env.NEXT_PUBLIC_API + '/words', {
     cache: 'no-cache'
-    // next:{
-    //   revalidate: 5
-    // }
   })
 
   const data = await res.json()
